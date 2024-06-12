bfb6213bbd62cbfcb6540a3a208912787301be8b
funnyJack <pengli.he@hkmci.com>
2024-06-12 17:06:01 +0800

    首页导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,49 @@ Page({
         interval:3000,
         duration:1000,
         swiperData:[]
-    }
+    },
+    navData:[
+      {
+          text:"数码",
+          icon:"like",
+          color:"#ff0000"
+      },
+      {
+          text:"生鲜",
+          icon:"star",
+          color:"#ff0000"
+      },
+      {
+          text:"会员",
+          icon:"fire",
+          color:"#ff0000"
+      },
+      {
+          text:"优惠",
+          icon:"gift",
+          color:"#ff0000"
+      },
+      {
+          text:"充值",
+          icon:"phone",
+          color:"#ff0000"
+      },
+      {
+          text:"领券",
+          icon:"gem",
+          color:"#ff0000"
+      },
+      {
+          text:"外卖",
+          icon:"gift-card",
+          color:"#ff0000"
+      },
+      {
+          text:"美食",
+          icon:"smile",
+          color:"#ff0000"
+      }
+  ],
   },
 
   /**
